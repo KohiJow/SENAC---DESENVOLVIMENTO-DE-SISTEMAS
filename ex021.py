dc6ade6435c9f3a42b7fbec08c3820b52c447cca
KohiJow <contatojmfr@gmail.com>
2024-09-09 16:11:43 -0300

    Atualização

--- ex021.py
+++ ex021.py
@@ -1,1 +1,7 @@
 #Faça um programa em python que abra e reproduza o áudio de um arquivo MP3.
+
+import pygame
+pygame.init()
+pygame.mixer.music.load("C:\\Users\\conta\\Downloads\\guanabara\\guanabara\\guanabara\\Python\\ex021.mp3")
+pygame.mixer.music.play()
+pygame.event.wait()
