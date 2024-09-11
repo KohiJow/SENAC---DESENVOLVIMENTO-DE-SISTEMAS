d8fe23039642239a6b4d491b89ffb5b806d2f365
KohiJow <contatojmfr@gmail.com>
2024-09-11 19:57:04 -0300

    Att

--- Professor Fernando/ex003.py
+++ Professor Fernando/ex003.py
@@ -1,7 +1,20 @@
 #Fazer um programa de contagem regressiva
 
+#contagem = 10
+
+#while contagem > 0:
+#    print(f'Contagem regressiva: {contagem}') 
+#    contagem -= 1
+
+#Correção
+
+import time  # Importa o módulo time para usar a função sleep
+
 contagem = 10
 
 while contagem > 0:
-    print(f'Contagem regressiva: {contagem}') 
+    print(f'Contagem regressiva: {contagem}')
+    time.sleep(1)  # Espera 1 segundo
     contagem -= 1
+
+print('Contagem regressiva concluída!')
