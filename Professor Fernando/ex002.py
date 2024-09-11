10b56ea367bbeb86e6bf2dafd7a11901e3c8940d
KohiJow <contatojmfr@gmail.com>
2024-09-11 16:50:04 -0300

    att

--- Professor Fernando/ex002.py
+++ Professor Fernando/ex002.py
@@ -1,14 +1,16 @@
 #Crie um programa que diga se uma pessoa pode dirigir ou não, com base nos valores idade, altura e nome.
 
-print('Olá seja bem vindo!')
+print('Olá, seja bem-vindo!')
 
-nome = (input('Qual seu nome: '))
+nome = input('Qual seu nome: ')
 print(f'Olá {nome}!')
 
 idade = int(input('Qual sua idade: '))
-altura = float(input('Digite sua altura: '))
+altura = float(input('Digite sua altura em metros: '))
 
-if idade < 18: 
-    print('Você ainda não tem idade suficiente')
-    
-else: print('Você pode dirigir!')
+if idade < 18:
+    print('Você ainda não tem idade suficiente para dirigir.')
+elif altura < 1.60:
+    print('Você não tem altura suficiente para dirigir.')
+else:
+    print('Você pode dirigir!')
---
+++ Professor Fernando/ex004.py
@@ -0,0 +1,7 @@
+#Tabuada com FOR
+
+num = int(input('Digite um número e eu mostrarei sua tabuada: '))
+
+for i in range(1,11):
+    print(num, 'x', i, '=', num * i)
+    
