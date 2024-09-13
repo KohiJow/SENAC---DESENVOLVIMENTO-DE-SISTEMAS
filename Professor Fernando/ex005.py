07c942c8bf066993fd5bb258ee6cd0b770bda3e8
KohiJow <contatojmfr@gmail.com>
2024-09-12 21:50:50 -0300

    att

--- Professor Fernando/ex005.py
+++ Professor Fernando/ex005.py
@@ -1,29 +1,77 @@
 #Calculadora
+while True:
+    a = int(input('Digite um valor: '))
 
-a = int(input('Digite um valor: '))
+    b = int(input('Digite outro valor: '))
 
-b = int(input('Digite outro valor: '))
+    soma = a + b
 
-soma = a + b
+    divisão = a / b
 
-divisão = a / b
+    subtração = a - b
 
-subtração = a - b
+    multiplicação = a * b
+    
+    escolha = str(input('Escolha qual operação deseja \n A)Subtração \n B)Divisão \n C)Multiplicação \n D)Soma\n E)Sair do programa \n R: ')).strip().upper()
 
-multiplicação = a * b
+    if escolha == 'A':
+        print(f'O Resultado da subtração de {a} por {b} é: {subtração}')
 
-escolha = str(input('Escolha qual operação deseja \n A)Subtração \n B)Divisão \n C)Multiplicação \n D)Soma')).split().upper()
+        continuar = str(input('Deseja continuar? Sim ou Não?\n')).upper().strip()
 
-if escolha == 'A':
-    print(subtração)
+        if  continuar == 'SIM':
+            print('continuando')
+        
+        elif continuar =='NAO' or 'NÃO':
+            break
 
-elif escolha == 'B':
-    print(divisão)
+        else:
+            print('Opção inválida, tente novamente: ')
+    
+    elif escolha == 'B':
+        print(f'O Resultado da divisão de {a} por {b} é: {divisão}')
 
-elif escolha == 'C':
-    print(multiplicação)
+        continuar = str(input('Deseja continuar? Sim ou Não?\n')).upper().strip()
 
-elif escolha == 'D':
-    print(soma)
+        if  continuar == 'SIM':
+            print('continuando')
+        
+        elif continuar =='NAO' or 'NÃO':
+            break
 
-#incompleto
+        else:
+            print('Opção inválida, tente novamente: ')
+
+    elif escolha == 'C':
+        print(f'O Resultado da multiplicação de {a} por {b} é: {multiplicação}')
+
+        continuar = str(input('Deseja continuar? Sim ou Não?\n')).upper().strip()
+
+        if  continuar == 'SIM':
+            print('continuando')
+        
+        elif continuar =='NAO' or 'NÃO':
+            break
+
+        else:
+            print('Opção inválida, tente novamente: ')
+
+    elif escolha == 'D':
+        print(f'O Resultado da soma de {a} por {b} é: {soma}')
+
+        continuar = str(input('Deseja continuar? Sim ou Não?\n')).upper().strip()
+
+        if  continuar == 'SIM':
+            print('continuando')
+        
+        elif continuar =='NAO' or 'NÃO':
+            break
+
+        else:
+            print('Opção inválida, tente novamente: ')
+
+    elif escolha == 'E':
+        break
+    
+    else:
+        print('Opção inválida, tente novamente: ')
