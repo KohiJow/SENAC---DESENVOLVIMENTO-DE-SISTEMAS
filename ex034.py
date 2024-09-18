10715e8b09da4e9c3f71784fc757b266f98a60f3
KohiJow <contatojmfr@gmail.com>
2024-09-17 21:58:11 -0300

    att

--- ex034.py
+++ ex034.py
@@ -1,3 +1,12 @@
 #Escreva um programa que pergunte o salário de um funcionário e calcule o valor de seu aumento.
 #Para salários superiores a R$1250, calcule um aumento de 10%
 #Para os inferiores ou iguais, o aumento é de 15%.
+
+salário = float(input('Insira seu salário. \nR: '))
+
+if salário <= 1250:
+    aumento = salário / 100 * 15 + salário
+    print(f'Seu novo salário é: ${aumento:.2f}') 
+else:
+    aumento = salário / 100 * 10 + salário
+    print(f'Seu novo salário é: ${aumento:.2f}')
