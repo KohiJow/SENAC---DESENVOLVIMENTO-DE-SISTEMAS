a86bd154eb5ec5c989f87280cc31666a26625588
KohiJow <contatojmfr@gmail.com>
2024-09-20 17:55:47 -0300

    att

--- ex043.py
+++ ex043.py
@@ -4,6 +4,22 @@ calcule seu IMC e mostre seu status de acordo com a tabela abaixo:
 -Abaixo de 18.5: Abaixo do peso
 -Entre 18.5 e 25: Peso ideal
 -25 até 30: Sobrepeso
--30 até 40? Obesodade
+-30 até 40? Obesidade
 -Acima de 40: Obesidade mórbida
 '''
+print('Por favor insira sua altura e peso: ')
+altura = float(input('Altura: '))
+peso = float(input('Peso: '))
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    print('Você está abaixo do peso')
+elif imc > 18.5 and  imc < 25:
+    print('Peso Ideal')
+elif imc > 20 and  imc < 30:
+    print('Sobrepeso')
+elif imc > 25 and  imc < 40:
+    print('Obesidade')
+else:
+    print('Obesidade Mórbida.')
+    
--- ex044.py
+++ ex044.py
@@ -4,5 +4,27 @@ produto, considerando o seu preço normal e condição de pagamento:
 -À vista dinheiro/cheque: 10% de desconto
 -À vista no cartão: 5% de desconto
 -Em até 2x no cartão: preço normal
--3x pu mais no cartão: 20% de juros
+-3x ou mais no cartão: 20% de juros
 '''
+
+print('Olá seja bem vindo ao caixa!')
+valor = float(input('Qual o valor da compra?\n'))
+fp = str(input('Qual a forma de pagamento? ')).strip().upper() 
+if fp == 'PARCELADO':
+    vezes = int(input('Em quantas vezes?\n'))
+    if vezes <=2:
+        print(f'valor total: {valor}')
+    else:
+        juros = (valor/100) * 20
+        print(f'valor total: {valor+juros}')
+    
+if fp == 'DINHEIRO' or fp == 'CHEQUE':
+    desconto = (valor/100) * 10
+    print(f'O valor a ser pago é {valor - desconto}')
+
+elif fp == 'CARTAO' or 'CARTÃO':
+    desconto = (valor/100) * 5
+    print(f'O valor a ser pago é {valor - desconto}')
+else:
+    print('Digite uma entrada válida')
+    
