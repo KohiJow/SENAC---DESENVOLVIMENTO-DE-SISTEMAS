67f29be6198dfa35d19eb87cb5c8f7f97c06d6bc
KohiJow <contatojmfr@gmail.com>
2024-10-02 17:04:11 -0300

    att

--- ex051.py
+++ ex051.py
@@ -1,1 +1,4 @@
-'''Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.'''
+'''
+Desenvolva um programa que leia o primeiro termo e a razão de uma PA. 
+No final, mostre os 10 primeiros termos dessa progressão.
+'''
