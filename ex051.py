6f975ea65beb995b81dda902e81e674e4b6b9da2
kohiJow <contatojmfr@gmail.com>
2024-10-02 22:00:41 -0300

    att

--- ex051.py
+++ ex051.py
@@ -2,3 +2,12 @@
 Desenvolva um programa que leia o primeiro termo e a razão de uma PA. 
 No final, mostre os 10 primeiros termos dessa progressão.
 '''
+razao = int(input('Qual sua razão: '))
+termo = int(input('Digite o primeiro termo: '))
+
+print('Os 10 primeiros termos são:')
+
+for i in range(10):
+    print(termo, end=' ')
+    termo += razao
+    
--- ex052.py
+++ ex052.py
@@ -1,1 +1,7 @@
 '''Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.'''
+for i in range(10):
+    num = int(input('Insira um número inteiro e direi se ele é ou não um número primo: ')=
+    if num > 1 and num % i != 0:
+        print('O número não é primo')
+    else:
+        print('O número é primo')
