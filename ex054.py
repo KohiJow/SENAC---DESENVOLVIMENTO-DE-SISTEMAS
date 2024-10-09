3f1cc80c5715424733aef812b2f230b8b9162ee6
KohiJow <contatojmfr@gmail.com>
2024-10-08 21:52:29 -0300

    att

--- ex054.py
+++ ex054.py
@@ -3,3 +3,13 @@ Crie um programa que leia o ano de nascimento de sete
 pessoas. No final, mostre quantas pessoas ainda não atingiram a 
 maioridade e quantas já são maiores.
 '''
+
+for i in range(1,8):
+    
+    idade = int(input(f'Insira a {i} idade: '))
+    
+    if idade < 18:
+        print(f'Você tem {idade} anos e ainda não atingiu a maioridade!')
+    else:
+        print(f'Você tem {idade} anos e já é de maior!')
+        
--- ex055.py
+++ ex055.py
@@ -1,1 +1,6 @@
 '''Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e menor peso lidos.'''
+pesos = []
+for i in range(1,6):
+    peso = float(input('Insira aqui seu peso: '))
+if peso
+    
