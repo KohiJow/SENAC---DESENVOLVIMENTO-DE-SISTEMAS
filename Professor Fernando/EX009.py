65b9da6ca9fa015225f05be45847ed2d26b33ee0
KohiJow <contatojmfr@gmail.com>
2024-10-18 17:45:26 -0300

    att

---
+++ Professor Fernando/EX009.py
@@ -0,0 +1,24 @@
+'''
+Veja que interessante, o Python tem a flexibilidade de capturar o valor de variáveis
+já declaradas ou não, onde vamos capturar dados mais adiante, neste exercicío, apenas
+execute o código a seguir e veja que lógica bacana deste algoritmo.
+
+Use a criatividade e melhore este código ;-)
+'''
+
+nome = 'Ana Maria Braga'
+idade = 17
+maior_de_idade = idade >= 18
+fim_do_programa = '--Sair do Sistema--'
+
+'''
+Calma! iremos aprender os operadores mais adiante, 
+este operador -> >= quer dizer MAIOR ou IGUAL
+'''
+
+print('')
+print('Nome completo:', nome, '-', 'Idade: ', idade)
+print('Ela é maior de idade?', maior_de_idade)
+print('') # Este print pula uma linha, o mesmo retorno do print \n
+print(fim_do_programa)
+
