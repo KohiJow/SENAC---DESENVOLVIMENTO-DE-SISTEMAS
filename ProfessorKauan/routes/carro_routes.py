e484d531519ec946bc4ccaa26e5a4e7fa2017c88
KohiJow <Contatojmfr@gmail.com>
2025-02-06 10:26:15 -0300

    att

--- ProfessorKauan/routes/carro_routes.py
+++ ProfessorKauan/routes/carro_routes.py
@@ -6,5 +6,6 @@ carro_routes = Blueprint('carro_routers',__name__)
 
 @carro_routes.route('/Carro', methods=['POST'])
 def carros_post():
+    carro_data = request.json
     return create_carro(request.json)
 
